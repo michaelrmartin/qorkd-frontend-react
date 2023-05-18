87e0dd75926ddbe75378a51d918d9855a84e4b2e
michaelrmartin <iammichaelmartin@gmail.com>
2023-05-18 16:01:38 -0500

    Update axios defaults

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import axios from 'axios'
-import './index.css'
-import "bootstrap"
-import "bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import axios from "axios";
+import "./index.css";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://qorkd.fly.dev";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://qorkd-v2.fly.dev";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
